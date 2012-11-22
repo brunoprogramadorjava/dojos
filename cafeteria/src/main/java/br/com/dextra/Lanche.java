e9ec5a1f70b4c5c688f4d04c2fd6a048567aa3bf
Fernando Ultremare <fernando@dextra-sw.com>
2012-11-22 13:32:13 -0200

    dojo 22-11-2012

--- cafeteria/src/main/java/br/com/dextra/Lanche.java
+++ cafeteria/src/main/java/br/com/dextra/Lanche.java
@@ -17,43 +17,43 @@ public abstract class Lanche {
 		for (Ingrediente ingredientes : this.ingredientes) {
 			calc += ingredientes.preco();
 		}
-		
+
 		return calc;
 	}
-	
+
 	public int contarIngrediente(Ingrediente ingrediente) {
 		int contador = 0;
 		for (Ingrediente ingredientes : this.ingredientes) {
-			if(ingredientes.equals(ingrediente)) {
+			if (ingredientes.equals(ingrediente)) {
 				contador++;
 			}
 		}
-		
+
 		return contador;
 	}
-	
+
 	public double getPreco() {
-		int countHamburguerCarne = contarIngrediente(Ingrediente.HamburguerCarne);
-		int countBacon = contarIngrediente(Ingrediente.Bacon);
-		
-		countHamburguerCarne = countHamburguerCarne / 2;
-		
-		double precoBacon = Ingrediente.Bacon.preco();
-		
-		return somarPrecoIngredientes() - precoBacon*Math.min(countBacon, countHamburguerCarne);
+		double precoSempromocao = somarPrecoIngredientes();
+
+		Promocao promocao = new Promocao(this);
+
+		if (promocao.seAplica())
+			return promocao.aplicar(precoSempromocao);
+		else
+			return precoSempromocao;
+
 	}
 
-	public void add(Ingrediente ingrediente){
+	public void add(Ingrediente ingrediente) {
 		this.ingredientes.add(ingrediente);
 	}
 
-	public void remove(Ingrediente ingrediente){
+	public void remove(Ingrediente ingrediente) {
 		this.ingredientes.remove(ingrediente);
 	}
-	
-	public boolean hasIngrediente(Ingrediente ingrediente){
+
+	public boolean hasIngrediente(Ingrediente ingrediente) {
 		return ingredientes.contains(ingrediente);
 	}
-	
 
 }
--- cafeteria/src/test/java/br/com/dextra/dilicia/CardapioTest.java
+++ cafeteria/src/test/java/br/com/dextra/dilicia/CardapioTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import br.com.dextra.Ingrediente;
 import br.com.dextra.LanchePersonalizado;
+import br.com.dextra.Promocao;
 import br.com.dextra.XBacon;
 import br.com.dextra.XEgg;
 import br.com.dextra.XFrango;
@@ -16,10 +17,10 @@ public class CardapioTest {
 
 	@Test
 	public void testPrecosCardapio() {
-		Assert.assertEquals(3.2, new XBacon().getPreco(), 0.01);
-		Assert.assertEquals(2.7, new XEgg().getPreco(), 0.01);
-		Assert.assertEquals(2.1, new XFrango().getPreco(), 0.01);
-		Assert.assertEquals(4.9, new XTudo().getPreco(), 0.01);
+		Assert.assertEquals(3.2*Promocao.PROMOCAO_SEJA_LIGHT_DESC, new XBacon().getPreco(), 0.01);
+		Assert.assertEquals(2.7*Promocao.PROMOCAO_SEJA_LIGHT_DESC, new XEgg().getPreco(), 0.01);
+		Assert.assertEquals(2.1*Promocao.PROMOCAO_SEJA_LIGHT_DESC, new XFrango().getPreco(), 0.01);
+		Assert.assertEquals(4.9*Promocao.PROMOCAO_SEJA_LIGHT_DESC, new XTudo().getPreco(), 0.01);
 	}
 
 	@Test
@@ -28,10 +29,11 @@ public class CardapioTest {
 		ingredientes.add(Ingrediente.Ovo);
 		ingredientes.add(Ingrediente.Alface);
 		ingredientes.add(Ingrediente.HamburguerCarne);
-
+		
 		LanchePersonalizado lancheNovo = new LanchePersonalizado(ingredientes);
-
-		Assert.assertEquals(1.9, lancheNovo.getPreco(), 0.01);
+		
+		
+		Assert.assertEquals((1.9*Promocao.PROMOCAO_SEJA_LIGHT_DESC), lancheNovo.getPreco(), 0.01);
 	}
 
 	@Test
@@ -45,7 +47,7 @@ public class CardapioTest {
 		lancheNovo.add(Ingrediente.Alface);
 		lancheNovo.add(Ingrediente.HamburguerCarne);
 
-		Assert.assertEquals(1.9, lancheNovo.getPreco(), 0.01);
+		Assert.assertEquals((1.9*Promocao.PROMOCAO_SEJA_LIGHT_DESC), lancheNovo.getPreco(), 0.01);
 	}
 
 	@Test
@@ -56,7 +58,7 @@ public class CardapioTest {
 		ingredientes.add(Ingrediente.HamburguerCarne);
 
 		LanchePersonalizado lancheNovo = new LanchePersonalizado(ingredientes);
-		Assert.assertEquals(1.9, lancheNovo.getPreco(), 0.01);
+		Assert.assertEquals(1.9*Promocao.PROMOCAO_SEJA_LIGHT_DESC, lancheNovo.getPreco(), 0.01);
 
 		lancheNovo.remove(Ingrediente.Alface);
 		lancheNovo.remove(Ingrediente.HamburguerCarne);
@@ -77,10 +79,9 @@ public class CardapioTest {
 					+ 2* Ingrediente.HamburguerCarne.preco();  
 		LanchePersonalizado lancheNovo = new LanchePersonalizado(ingredientes);
 		
-		Assert.assertEquals(preco , lancheNovo.getPreco(), 0.01);		
+		Assert.assertEquals(preco, lancheNovo.getPreco(), 0.01);		
 	}
 	
-
 	@Test
 	public void promocaoDoubleCarneSemBacon(){
 		ArrayList<Ingrediente> ingredientes = new ArrayList<Ingrediente>();
@@ -93,10 +94,9 @@ public class CardapioTest {
 					+ 2* Ingrediente.HamburguerCarne.preco();  
 		LanchePersonalizado lancheNovo = new LanchePersonalizado(ingredientes);
 		
-		Assert.assertEquals(preco , lancheNovo.getPreco(), 0.01);		
+		Assert.assertEquals(preco*Promocao.PROMOCAO_SEJA_LIGHT_DESC , lancheNovo.getPreco(), 0.01);		
 	}
 	
-
 	@Test
 	public void promocaoQuatroHamburgueresDeCarneEUmBacon(){
 		ArrayList<Ingrediente> ingredientes = new ArrayList<Ingrediente>();
@@ -108,10 +108,32 @@ public class CardapioTest {
 		ingredientes.add(Ingrediente.HamburguerCarne);
 		ingredientes.add(Ingrediente.Bacon);
 		
-		Double preco = Ingrediente.Alface.preco() + Ingrediente.Queijo.preco() 
-					+ 4 * Ingrediente.HamburguerCarne.preco();  
+		Double preco = precoSemCobrarOBacon();  
 		LanchePersonalizado lancheNovo = new LanchePersonalizado(ingredientes);
 		
-		Assert.assertEquals(preco , lancheNovo.getPreco(), 0.01);		
+		Assert.assertEquals(preco, lancheNovo.getPreco(), 0.01);		
 	}
+
+	private Double precoSemCobrarOBacon() {
+		Double preco = Ingrediente.Alface.preco() + Ingrediente.Queijo.preco() 
+					+ 4 * Ingrediente.HamburguerCarne.preco();
+		return preco;
+	}
+
+	@Test
+	public void promocaoLightLancheComAlface() {
+		ArrayList<Ingrediente> ingredientes = new ArrayList<Ingrediente>();
+		ingredientes.add(Ingrediente.Alface);
+		ingredientes.add(Ingrediente.HamburguerCarne);
+		ingredientes.add(Ingrediente.Queijo);
+		
+		Double preco = Ingrediente.Queijo.preco() + Ingrediente.HamburguerCarne.preco() 
+			+ Ingrediente.Alface.preco();
+		preco = preco*Promocao.PROMOCAO_SEJA_LIGHT_DESC;
+		
+		LanchePersonalizado 
+		lancheNovo = new LanchePersonalizado(ingredientes);
+		Assert.assertEquals(preco , lancheNovo.getPreco(), 0.01);
+		
+	}	
 }
